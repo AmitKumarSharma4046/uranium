4705555639fd822980d07be71f05507ea9f37e0c
AmitKumarSharma4046 <aerez4046@gmail.com>
2022-04-13 01:48:59 +0530

    changes

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -7,9 +7,36 @@ const createBook= async function (req, res) {
 }
 
 const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find()
+    let allBooks= await BookModel.find().select({bookName:1,authorName:1,_id:0})
     res.send({msg: allBooks})
 }
 
+const getBooksInYear= async function (req, res) {
+    let yr=req.body
+    let allBooks= await BookModel.find(yr)
+    res.send({msg: allBooks})
+}
+
+const getParticularBooks= async function (req, res) {
+    let allBooks= await BookModel.find({$or:[{bookName:req.body.bookName},{authorName:req.body.authorName},{tags:req.body.tags},
+        {year:req.body.year},{price:req.body.price},{totalPages:req.body.totalPages},{stockAvailable:req.body.stockAvailable}]})
+    res.send({msg: allBooks})
+}
+
+const getXINRBooks= async function (req, res) {
+    let allBooks= await BookModel.find({"price.indian":{$in:[100,200,500]}})
+    res.send({msg: allBooks})
+}
+
+const getRandomBooks= async function (req, res) {
+    let allBooks= await BookModel.find({$or:[{stockAvailable:{$eq:true}},{totalPages:{$gt:500}}]})
+    res.send({msg: allBooks})
+}
+
+
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+module.exports.getBooksData= getBooksData 
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks=getParticularBooks
+module.exports.getXINRBooks=getXINRBooks
+module.exports.getRandomBooks=getRandomBooks
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { stringify } = require('nodemon/lib/utils');
 
 // const userSchema = new mongoose.Schema( {
 //     firstName: String,
@@ -26,8 +27,14 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema( {
     bookName: {type:String,required:true},
     authorName: {type:String,required:true},
-    category:String,
-    year: {type:Number,required:true},
+    tags:[String],
+    year: {type:Number,required:true,default:2021},
+    price:{
+        indian : String,
+        european : String
+    },
+    totalPages : Number,
+    stockAvailable : Boolean
 }, { timestamps: true });
 
 
